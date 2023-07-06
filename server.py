638e5607a896206d1421c2944d6250936cd28cd4
Siraj 'Sid' Rakhada <sid-git@mindless.co.uk>
2023-07-06 20:36:00 +1000

    WIP for server.py, vscode stuff, and some battery errors

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-
-#Server to fake www.alphaess.com port 7777 that batteries sync data to
+""" Threaded server to fake www.alphaess.com port 7777 
+that batteries sync data to"""
 
 import binascii
 import socket
@@ -8,9 +8,10 @@ import struct
 import sys
 import re
 import json
+from datetime import datetime
+
 from crccheck.crc import Crc16Modbus
 
-from datetime import datetime
 
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
@@ -18,18 +19,22 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 
 HOST, PORT = "localhost", 7778
 
+
 def server_program():
-    server_socket = socket.socket() # get instance
+    """Main server loop"""
+
+    server_socket = socket.socket()  # get instance
     # look closely. The bind() function takes tuple as argument
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((HOST, PORT))  # bind host address and port together
 
-    # configure how many client the server can listen simultaneously
+    # configure how many clients the server can listen to simultaneously
     server_socket.listen(1)
     conn, address = server_socket.accept()  # accept new connection
     print("Connection from: " + str(address))
     while True:
+        pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     server_program()
